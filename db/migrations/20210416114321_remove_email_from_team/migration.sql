1c5f7ec17fea9e231cdc8afec2dce5afade007c1
Gunnar Torfi <gunnartorfi@gmail.com>
2021-04-16 22:57:27 +0000

    Remove old migrations

--- db/migrations/20210416114321_remove_email_from_team/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `email` on the `Team` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Team" DROP COLUMN "email";
