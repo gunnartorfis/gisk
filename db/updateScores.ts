21a68f6f5de8a3ab19837a850d5ff808191a10ee
Gunnar Torfi Steinarsson <gunnartorfis@gmail.com>
2022-11-15 11:53:33 +0000

    Only update match results if the match is completed

--- db/updateScores.ts
+++ db/updateScores.ts
@@ -18,20 +18,21 @@ const updateScores = async () => {
       return;
     }
 
-
-    await db.match.update({
-      where: {
-        homeTeamId_awayTeamId_kickOff: {
-          homeTeamId: hometeam?.id as string,
-          awayTeamId: awayTeam?.id as string,
-          kickOff: newMatch.datetime
-        }
-      },
-      data: {
-        resultHome: newMatch.home_team.goals ?? 0,
-        resultAway: newMatch.away_team.goals ?? 0,
-      },
-    })
+    if (newMatch.status === 'completed') {
+      await db.match.update({
+        where: {
+          homeTeamId_awayTeamId_kickOff: {
+            homeTeamId: hometeam?.id as string,
+            awayTeamId: awayTeam?.id as string,
+            kickOff: newMatch.datetime
+          }
+        },
+        data: {
+          resultHome: newMatch.home_team.goals ?? 0,
+          resultAway: newMatch.away_team.goals ?? 0,
+        },
+      }) 
+    }
   })
 }
 
