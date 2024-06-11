dc8d1899f379ba8b6b87309a38787bbcbb15c1ed
Siggi Njals <siggi.njals@dohop.com>
2024-06-11 11:05:21 +0000

    Updating cronjob

---
+++ db/results.ts
@@ -0,0 +1,110 @@
+export const results = {
+  Stages: [
+    {
+      Sid: '15705',
+      Snm: 'Group A',
+      Scd: 'group-a',
+      badgeUrl: '2024-euro.png',
+      firstColor: '0008ff',
+      Cnm: 'Euro 2024',
+      CnmT: 'euro-2024',
+      Csnm: 'Euro 2024',
+      Ccd: 'euro-2024',
+      CompId: '492',
+      CompN: 'Euro 2024',
+      CompD: 'UEFA',
+      Scu: 0,
+
+      Events: [
+        {
+          Eid: '1085833',
+          Pids: {
+            '12': 'SBTE_1_29800728',
+            '112': 'SBTE_2_1020306930',
+            '29': '1085833',
+            '8': '1085833',
+          },
+          Media: {
+            '112': [
+              {
+                eventId: 'ITV 1',
+                provider: 'ABELSON',
+                type: 'TV_CHANNEL',
+                allowedCountries: ['GB'],
+              },
+              {
+                eventId: 'ITVX',
+                provider: 'ABELSON',
+                type: 'TV_CHANNEL',
+                allowedCountries: ['GB'],
+              },
+              {
+                eventId: 'NPO 1',
+                provider: 'ABELSON',
+                type: 'TV_CHANNEL',
+                allowedCountries: ['NL'],
+              },
+              {
+                eventId: 'RTE 2',
+                provider: 'ABELSON',
+                type: 'TV_CHANNEL',
+                allowedCountries: ['IE'],
+              },
+              {
+                eventId: 'UTV',
+                provider: 'ABELSON',
+                type: 'TV_CHANNEL',
+                allowedCountries: ['GB', 'IE'],
+              },
+            ],
+            '29': [
+              {
+                streamhls:
+                  'https://livesportradio.com/global/reeng16/playlist.m3u8',
+                deniedCountries: [],
+                ageRestricted: 'false',
+                beforeStartTime: 5,
+                afterEndTime: 15,
+              },
+            ],
+          },
+          T1: [
+            {
+              Nm: 'Germany',
+              ID: '9016',
+              Img: 'enet/8570.png',
+              NewsTag: '/team/germany-2021020913320920836-1272/',
+              Abr: 'GER',
+            },
+          ],
+          T2: [
+            {
+              Nm: 'Scotland',
+              ID: '9029',
+              Img: 'enet/8498.png',
+              NewsTag: '/team/scotland-2022062716001253418-1689/',
+              Abr: 'SCO',
+            },
+          ],
+          Eps: 'NS',
+          Esid: 1,
+          Epr: 0,
+          Ecov: 0,
+          ErnInf: '1',
+          Et: 1,
+          Esd: 20240614190000,
+          EO: 345819175,
+          EOX: 345819175,
+          Spid: 1,
+          Pid: 1,
+          Tr1: '2',
+          Tr2: '1',
+          Trh1: '0',
+          Trh2: '0',
+          Tr1OR: '2',
+          Tr2OR: '1',
+        },
+      ],
+    },
+  ],
+};
